ae0c0508c39742be89bbd428fb0939f3bdab8b5d
Owner <you@example.com>
2026-01-21 11:58:12 -0500

    first commit

---
+++ Demo1.py
@@ -0,0 +1,9 @@
+#### concatenate the different datatype in one print method
+
+print("{},{},{}".format(1,"Hello",4))
+#1,Hello
+
+#### concatenate the same datatype in one print method
+print("Class"+"A")
+print("Class"+str(5))
+print("{},{}".format("class",5))
